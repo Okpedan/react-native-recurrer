921029b78eb4da9b5dee67a0df7393912e739054
Daniel Okpe <okpedan@gmail.com>
2026-04-05 21:42:26 +0100

    Added customised fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,29 +8,29 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
+      <Text className="text-5xl font-sans-extrabold">
+        Home
       </Text>
 
-      <Link href="/onboarding" className="border rounded-full p-4 bg-accent">
+      <Link href="/onboarding" className="border rounded-full p-4 bg-accent m-1">
         <Text className="text-xl font-bold text-white">
           Go to Onboarding
         </Text>
       </Link>
 
-      <Link href="/(auth)/sign-in" className="border rounded-full p-4 bg-accent">
+      <Link href="/(auth)/sign-in" className="border rounded-full p-4 bg-accent m-1">
         <Text className="text-xl font-bold text-white">
           Sign In
         </Text>
       </Link>
 
-      <Link href="/(auth)/sign-up" className="border rounded-full p-4 bg-accent">
+      <Link href="/(auth)/sign-up" className="border rounded-full p-4 bg-accent m-1">
         <Text className="text-xl font-bold text-white">
           Sign Up
         </Text>
       </Link>
 
-      <Link href="/(tabs)/settings" className="border rounded-full p-4 bg-accent">
+      <Link href="/(tabs)/settings" className="border rounded-full p-4 bg-accent m-1">
         <Text className="text-xl font-bold text-white">
           Go to Settings
         </Text>
